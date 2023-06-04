e8387949748182b97523ca4760d7e0a53422fc4d
Pravesh <praveshjha5@gmail.com>
2023-06-04 11:09:23 +0530

    including both plans

--- src/components/MyPlanModal.js
+++ src/components/MyPlanModal.js
@@ -50,11 +50,11 @@ const MyPlanModal = ({ open, handleClose }) => {
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth={modalMaxWidth}>
       <Typography style={{ padding: "10px", fontSize: "16px", fontWeight: "bold" }}>
-        Currently, you are on the Learnometry Free plan. Upgrade to Learnometry Pro to enjoy unlimited daily responses and gain access to additional features.
+        Currently, you are on the Learnometry Free plan. Upgrade your plan to enjoy unlimited daily responses and gain access to additional features.
       </Typography>
       <DialogContent>
         <Grid container spacing={gridSpacing}>
-          {/* <Grid item xs={gridColumns}>
+          <Grid item xs={gridColumns}>
             <Box sx={{ height: '100%' }}>
               <Paper elevation={1} sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
                 <Typography variant="h5" align="center" gutterBottom
@@ -82,14 +82,14 @@ const MyPlanModal = ({ open, handleClose }) => {
                   </List>
                 </Box>
                 <Box sx={{ mt: 'auto', display: "flex", alignItems: "center", justifyContent: "center" }}>
-                  <Button variant="contained" color="primary" onClick={handleGetPlus}
+                  <Button variant="contained" color="primary" onClick={handleGetPro}
                     style={{ backgroundColor: 'black', color: 'white' }}>
                     Get Plus
                   </Button>
                 </Box>
               </Paper>
             </Box>
-          </Grid> */}
+          </Grid>
           <Grid item xs={gridColumns}>
             <Box sx={{ height: '100%' }}>
               <Paper elevation={1} sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
@@ -102,7 +102,7 @@ const MyPlanModal = ({ open, handleClose }) => {
                   Pro
                 </Typography>
                 <Typography variant="h6" style={{ fontWeight: "bold" }} gutterBottom>
-                  $15/month
+                  $20/month
                 </Typography>
                 <Box sx={{ flexGrow: 1 }}>
                   <List>
