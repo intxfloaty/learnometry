5f8dd5e74696748251e5e1ae0d45499517230391
Pravesh <praveshjha5@gmail.com>
2023-06-05 14:09:52 +0530

    initial UI design for stacks page

--- src/pages/stacks.js
+++ src/pages/stacks.js
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Sidebar from '@/components/Sidebar'
-import styles from '@/styles/Home.module.css'
-import { fetchStacks } from '@/utils/firebase'
-import { Grid, Card, CardContent, Typography, useMediaQuery, useTheme } from '@mui/material'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import Sidebar from '@/components/Sidebar';
+import { fetchStacks } from '@/utils/firebase';
+import Link from 'next/link';
+import styles from '@/styles/Stacks.module.css';
 
 function Stacks() {
   const [stacks, setStacks] = useState([]);
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,29 +17,20 @@ function Stacks() {
 
   return (
     <main className={styles.main}>
-      <Sidebar />
-      <Grid container spacing={3} style={{ marginLeft: "10px" }}>
-        {stacks.map((stack) => (
-          <Grid item xs={12} sm={6} md={4} key={stack.id}>
-            <Link href={`/stacks/${stack.id}`} passHref>
-              <Card sx={{
-                maxWidth: isSmallScreen ? '100%' : 345,
-                margin: 'auto',
-                boxShadow: 3,
-              }}>
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {stack.stackName}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {/* Additional information can be added here */}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Link>
-          </Grid>
+      <div>
+        <Sidebar />
+      </div>
+      <div className={styles.stacksContainer}>
+        {stacks.map((stack, index) => (
+          <Link href={`/stacks/${stack.id}`} passHref key={index}>
+            <div className={styles.stack}>
+              <span className={styles.stackName}>
+                {stack.stackName}
+              </span>
+            </div>
+          </Link>
         ))}
-      </Grid>
+      </div>
     </main>
   )
 }
