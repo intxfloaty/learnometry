d624b986dee2a62155bf2279e8f5eafc155efff2
Pravesh <praveshjha5@gmail.com>
2023-06-20 15:37:09 +0530

    so that only Plus plan is available for now

--- src/components/MyPlanModal.js
+++ src/components/MyPlanModal.js
@@ -195,8 +195,8 @@ const MyPlanModal = ({ open, handleClose, modalMessage }) => {
           Currently, you are on the Learnometry Free plan. Upgrade your plan to enjoy unlimited daily responses and gain access to additional features.
         </Typography>}
       <DialogContent>
-        <Grid container spacing={gridSpacing}>
-          <Grid item xs={gridColumns}>
+        <Grid container spacing={gridSpacing} justifyContent="center">
+          <Grid item xs={12} sm={6} md={4}>
             <Box sx={{ height: '100%' }}>
               <Paper elevation={1} sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
                 <Typography variant="h5" align="center" gutterBottom
@@ -229,52 +229,6 @@ const MyPlanModal = ({ open, handleClose, modalMessage }) => {
                     disabled={plusLink}>
                     {!plusLink
                       ? "Get Plus"
-                      : <CircularProgress color="inherit" size={24} />
-                    }
-                  </Button>
-                </Box>
-              </Paper>
-            </Box>
-          </Grid>
-          <Grid item xs={gridColumns}>
-            <Box sx={{ height: '100%' }}>
-              <Paper elevation={1} sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <Typography variant="h5" align="center" gutterBottom
-                  style={{
-                    color: "blue",
-                    fontSize: "28px",
-                    fontWeight: "bolder"
-                  }} >
-                  Pro
-                </Typography>
-                <Typography variant="h6" style={{ fontWeight: "bold" }} gutterBottom>
-                  $20/month
-                </Typography>
-                <Box sx={{ flexGrow: 1 }}>
-                  <List>
-                    <ListItem disablePadding>
-                      <Typography style={listItemStyle} align="center">1. Unlimited Responses/day</Typography>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography style={listItemStyle} align="center">2. Different learning Styles</Typography>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography style={listItemStyle} align="center">3. Stack history</Typography>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography style={listItemStyle} align="center">4.Chat with PDF</Typography>
-                    </ListItem>
-                    <ListItem disablePadding>
-                      <Typography style={listItemStyle} align="center">5. Access to upcoming new features</Typography>
-                    </ListItem>
-                  </List>
-                </Box>
-                <Box sx={{ mt: 'auto', display: "flex", alignItems: "center", justifyContent: "center" }}>
-                  <Button variant="contained" color="primary" onClick={handleGetPro}
-                    style={{ backgroundColor: 'black', color: 'white' }}
-                    disabled={proLink}>
-                    {!proLink
-                      ? "Get Pro"
                       : <CircularProgress color="inherit" size={24} />
                     }
                   </Button>
